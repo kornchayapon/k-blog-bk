f27464df7bc6c757e32cf432e04a408fdf10810b
chayapon <chayapon@MacBook-Pro-khxng-chayapon.local>
2025-11-24 10:15:21 +0700

    23-2 - Set refresh-token to cookie in login controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { AuthService } from './providers/auth.service';
 import { SignInDto } from './dtos/signin.dto';
 import { RefreshTokenDto } from './dtos/refresh-token.dto';
 import { Auth } from './decorators/auth.decorator';
 import { AuthType } from './enums/auth-type.enum';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -15,8 +23,22 @@ export class AuthController {
   @Post()
   @HttpCode(HttpStatus.OK)
   @Auth(AuthType.None)
-  public signIn(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto);
+  public async signIn(
+    @Body() signInDto: SignInDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken, refreshToken, user } =
+      await this.authService.signIn(signInDto);
+
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax',
+      path: '/',
+      maxAge: 7 * 24 * 3600 * 1000,
+    });
+
+    return { access_token: accessToken, user };
   }
 
   @Post('refresh-tokens')
